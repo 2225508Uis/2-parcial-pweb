452bce6aee6ddad2e034408a336fceee787e076c
Jhon Alexander Niño Cordero <jhonalexander20011026@gmail.com>
2026-04-15 09:49:49 -0500

    se añadio la visualizacion en index e indexadmin

--- modulo/views.py
+++ modulo/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .models import Distro
+from .forms import TaskForm
+
 
 # Create your views here.
 def index(request):
+    distros = Distro.objects.all().order_by('-created_at')
+    context = {
+        'distros': distros,
+        'emoji_done': '✅',
+        'emoji_pending': '⛔'
+    }
+
     if request.user.is_authenticated:
-        return render(request, 'modulo/indexadmin.html')
+        return render(request, 'modulo/indexadmin.html', context)
     else:
-        return render(request, 'modulo/index.html')
+        return render(request, 'modulo/index.html', context)
 
 def login_view(request):
     if request.method == 'POST':
